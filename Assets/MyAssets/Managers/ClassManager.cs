271b89681255e568420e86f72e814e97869458cd
Jose Marco Nufio Garrido <jose.m.nufio.0@gmail.com>
2024-04-02 02:38:29 +0200

    Started dividing the work of `TestClassManager`

---
+++ Assets/MyAssets/Managers/ClassManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(ClassMiniGamePromptManager))]
+public class ClassManager : MonoBehaviour {
+	public ClassMiniGamePromptManager mini_game_prompt_manager { get; private set; }
+	public ClassData[] classes;
+	private int active_class_index = 0;
+
+	public Option<ClassData> current_class() {
+		int classes_count = classes.Length;
+		if (classes_count == 0) return Option<ClassData>.None;
+		if (active_class_index < 0 || active_class_index >= classes_count) {
+			return Option<ClassData>.None;
+		}
+		ClassData data = classes[active_class_index];
+		return Option<ClassData>.Some(data);
+	}
+
+	private void Awake() {
+		mini_game_prompt_manager = GetComponent<ClassMiniGamePromptManager>();
+	}
+
+	private void Start() {
+		current_class().match<object>(
+			(data) => mini_game_prompt_manager.data = data,
+			() => null
+		);
+	}
+}
---
+++ Assets/MyAssets/Managers/ClassMiniGamePromptManager.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClassMiniGamePromptManager : MonoBehaviour {
+	[SerializeField]
+	private UITextPanel text_prompt_panel;
+	[SerializeField]
+	private UITextPanel question_prompt_panel;
+	[SerializeField]
+	private UITextPanel class_done_panel;
+	public ClassData data;
+}
